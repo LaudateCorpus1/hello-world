820eb8398c119c3340c99fd197aa9f31577ba38b
peterfraedrich <peterfraedrich@gmail.com>
2021-10-18 09:58:33 -0400

    move project_id to env var instead of hardcode

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ db_host = os.environ.get("DB_HOST", "localhost")
 db_user = os.environ.get("DB_USER", "postgres")
 db_password = os.environ.get("DB_PASSWORD", "1234")
 db_name = os.environ.get("DB_NAME", "hello-world-db")
+project_id = os.environ.get("GCP_PROJECT_ID", "idme-328822")
 
 app = Flask(__name__)
 db = peewee.PostgresqlDatabase(db_name, host=db_host, user=db_user, password=db_password)
@@ -66,7 +67,7 @@ def init_tracer():
         return
     logging.debug("Initializing OpenCensus tracer")
     exporter = stackdriver_exporter.StackdriverExporter(
-        project_id='idme-328822',
+        project_id=project_id,
     )
     tracer = opencensus.trace.tracer.Tracer(
         exporter=exporter,
